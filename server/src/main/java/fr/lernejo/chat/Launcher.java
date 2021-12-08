4aaecee30a5d079f8616984b3b20ac7bb2450a9a
EdMkn <nmanyim@rocketmail.com>
2021-12-08 18:00:23 +0100

    part7

---
+++ server/src/main/java/fr/lernejo/chat/Launcher.java
@@ -0,0 +1,27 @@
+package fr.lernejo.chat;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class Launcher {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Launcher.class);
+        RabbitTemplate template = context.getBean(RabbitTemplate.class);
+        template.setRoutingKey("chat_messages");
+
+        while(true){
+            System.out.println("Input a message, we will sent it for you (q for quit)");
+            String rponse = scan.nextLine();
+            if(scan.equals("q")){
+                System.out.println("Goodbye.");
+                System.exit(0);
+            }
+            template.convertAndSend((scan));
+            System.out.println("Message sent.");
+        }
+    }
+}
